680c473e2b21cff953a2902422d2185d6d3b9e98
Muh-Uzair <muhammaduzair1062001@gmail.com>
2024-05-15 14:31:58 +0500

    sidebar complete Alhamdulillah

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,51 @@ import LogIn from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CitiesList from "./components/appLayout/CitiesList";
 import CountryList from "./components/appLayout/CountryList";
+import { URL } from "../api-related/apiRelated";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [citiesData, set_citiesData] = useState([]);
+  const [isLoading, set_isLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCitiesData() {
+      try {
+        set_isLoading(true);
+        const res = await fetch(`${URL}/cities`);
+        const data = await res.json();
+        set_citiesData(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        set_isLoading(false);
+      }
+    }
+    fetchCitiesData();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="appLayout" element={<AppLayout />}>
-          <Route index element={<CitiesList />} />
-          <Route path="cities" element={<CitiesList />} />
-          <Route path="countries" element={<CountryList />} />
+          <Route
+            index
+            element={
+              <CitiesList citiesData={citiesData} isLoading={isLoading} />
+            }
+          />
+          <Route
+            path="cities"
+            element={
+              <CitiesList citiesData={citiesData} isLoading={isLoading} />
+            }
+          />
+          <Route
+            path="countries"
+            element={
+              <CountryList citiesData={citiesData} isLoading={isLoading} />
+            }
+          />
         </Route>
         <Route path="Pricing" element={<Pricing />} />
         <Route path="Product" element={<Product />} />
--- src/components/appLayout/CitiesList.jsx
+++ src/components/appLayout/CitiesList.jsx
@@ -1,12 +1,42 @@
-export default function CitiesList() {
+import PropTypes from "prop-types";
+import styles from "../appLayout/appLayout.module.css";
+
+CitiesList.propTypes = {
+  citiesData: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+function formatDate(inputDate) {
+  const date = new Date(inputDate);
+  const options = { day: "2-digit", month: "long", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+  let formattedDateString = formattedDate.replace(",", "");
+  const lastIndex = formattedDateString.lastIndexOf(" ");
+  formattedDateString =
+    formattedDateString.slice(0, lastIndex) +
+    ", " +
+    formattedDateString.slice(lastIndex + 1);
+
+  return `(${formattedDateString})`;
+}
+
+export default function CitiesList({ citiesData, isLoading }) {
   return (
-    <div>
-      <ul>
-        <li>London</li>
-        <li>New York</li>
-        <li>Lisbon</li>
-        <li>Peshawar</li>
-      </ul>
-    </div>
+    <>
+      {isLoading && <img className={styles.imgSpinner} src="Spinner.gif" />}
+      {!isLoading && (
+        <ul className={styles.CitiesListUl}>
+          {citiesData?.map((val, i) => (
+            <li key={i}>
+              <span className={styles.countryEmoji}>{val.emoji}</span>
+              <h3 className={styles.cityName}>{val.cityName}</h3>
+              <time className={styles.visitDate}>{formatDate(val.date)}</time>
+
+              <button className={styles.closeButton}>&times;</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
